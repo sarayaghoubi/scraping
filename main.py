4b68a59142d99c7846b46ad1bc8f6f076a383f52
sara <sara@sara>
2022-06-19 19:40:45 +0430

    added plot function

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import config
 from tguscraper import TGUScraper as Tgu
 import psycopg2
+import config
 
 if __name__ == '__main__':
 
     conn = psycopg2.connect(database='tgu', user='postgres', password='1372', host='127.0.0.1', port='5432')
     conn.autocommit = True
-    selenium = Tgu("https://www.tgju.org/profile/{s}/history", None, conn)
-    selenium.historical_data_wrapper('price_dollar_rl', '1400/03/01', '1401/03/25')
+    selenium = Tgu(config.url, None, conn)
+    selenium.historical_data_wrapper(config.symbol_to_search, config.from_date, config.to_date)
+    selenium.show_table(config.table_name)
+    print("final")
+
--- tguscraper.py
+++ tguscraper.py
@@ -3,10 +3,8 @@ from selenium.webdriver.common.by import By
 from base_db import DBHandler
 import time
 import config
-import matplotlib.pyplot as plt
-from mplfinance import candlestick_ohlc
+import mplfinance as mlp
 import pandas as pd
-import matplotlib.dates as mpdates
 
 
 class TGUScraper(ScrapperSelenium):
@@ -15,12 +13,9 @@ class TGUScraper(ScrapperSelenium):
         super(TGUScraper, self).__init__()
         self.historical_data_url = h_url
         self.db = DBHandler(db_name, con)
-        self.show_modes = {
-            'chart': self.show_chart,
-            'line': self.plot_data
-        }
 
     def historical_data_wrapper(self, symbol, date_from, date_to):
+        sleep_time = 2
         self.driver.get(self.historical_data_url.format(s=symbol))
         date_from_id = config.date_from_id
         date_to_id = config.date_to_id
@@ -37,9 +32,13 @@ class TGUScraper(ScrapperSelenium):
             table = self.fetch_element(By.ID, price_list_id)
             table_str = table.text
             self.insert_page_to_db(table_str, config.table_name)
-            self.driver.execute_script("arguments[0].click();", next_btn)
-            time.sleep(2)
-
+            try:
+                self.driver.execute_script("arguments[0].click();", next_btn)
+            except:
+                next_btn = self.fetch_element(By.ID, config.btn_next)
+                self.driver.execute_script("arguments[0].click();", next_btn)
+                sleep_time += 1
+            time.sleep(sleep_time)
             next_btn = self.fetch_element(By.ID, config.btn_next)
 
     @staticmethod
@@ -48,59 +47,26 @@ class TGUScraper(ScrapperSelenium):
         etc_str = string.index('…') + 1
         return int(string[etc_str:next_btn])
 
-    def insert_page_to_db(self, tbl, table, save_as_csv):
+    def insert_page_to_db(self, tbl, table):
         tbl = tbl.split('\n')
-        df = pd.DataFrame(columns=[config.price_keywords, 'Date'])
         for line in tbl:
             if line[0] != '0':
                 line = line.replace('/', '-')
-                line = line.replace(',', '.').split(' ')
-                row = f'{line[0]},{line[1]},{line[2]},{line[3]},\'{line[6]}\',\'{line[7]}\''
+                line = line.replace(',', '').split(' ')
+                row = f'{line[0]},{line[2]},{line[1]},{line[3]},\'{line[6]}\',\'{line[7]}\''
                 self.db.insert_row(table, row)
-                df = df.append(float(line[0]), float(line[1]), float(line[2]), float(line[3]), line[7],
-                               ignore_index=True)
-        if save_as_csv is not None:
-            df.to_csv(save_as_csv, index=False)
 
-    @staticmethod
-    def show_chart(data):
-        plt.style.use('dark_background')
-        # extracting Data for plotting
-        df = pd.read_csv(f'{data}.csv')
-        df = df[['OPEN', 'LOW', 'HIGH',
-                 'CLOSE', 'DateF']]
-        # convert into datetime object
-        df['DateF'] = pd.to_datetime(df['DateF'])
-        # apply map function
-        df['DateF'] = df['DateF'].map(mpdates.date2num)
-        # creating Subplots
-        fig, ax = plt.subplots()
-        # plotting the data
-        candlestick_ohlc(ax, df.values, width=0.6,
-                         colorup='green', colordown='red',
-                         alpha=0.8)
-        # allow grid
-        ax.grid(True)
-
-        # Setting labels
-        ax.set_xlabel('Date')
-        ax.set_ylabel('Price')
-
-        # setting title
-        plt.title('Dollar to Rial')
-
-        # Formatting Date
-        date_format = mpdates.DateFormatter('%d-%m-%Y')
-        ax.xaxis.set_major_formatter(date_format)
-        fig.autofmt_xdate()
-        fig.tight_layout()
-        # show the plot
-        plt.show()
-
-    @staticmethod
-    def plot_data(data, price_keyword):
-        assert price_keyword in config.price_keywords
-        # plotting a line graph
-        df = pd.read_csv(f'{data}.csv')
-        plt.plot(df["DateF"], df[price_keyword])
-        plt.show()
+    def show_table(self, table):
+        df = self.db.sql_to_df(table)
+        df.datee = pd.to_datetime(df.datee)
+        df = df.set_index('datee')
+        df = df.drop('datef', axis=1)
+        df.rename(columns={"open": "Open", "high": "High", "low": "Low", "close": "Close"},
+                  inplace=True)
+        df = df.iloc[::-1]
+        ap = mlp.make_addplot(df['Close'])
+        mlp.plot(
+            df,
+            type='candle',
+            addplot=ap
+        )
